495d9e79a448c82ff6ea93836211a46a46d0e2ed
Philip Doyle <doyle81@uwindsor.ca>
2023-11-29 01:31:51 -0500

    completed UI design and added a search bar on product page

--- fiction2form_frontend/src/ProductPage.tsx
+++ fiction2form_frontend/src/ProductPage.tsx
@@ -4,14 +4,14 @@ import './ProductPage.css';
 const ProductPage: React.FC = () => {
     return (
         <>
-        <h1>this is the product page</h1>
-            <div className={"flex-container"}>
-                <div className={"flex-item1"}>Animals</div>
-                <div className={"flex-item2"}>Geometry</div>
-                <div className={"flex-item3"}>Vehicles</div>
-                <div className={"flex-item4"}>Characters</div>
-                <div className={"flex-item5"}>Architecture</div>
-            </div>
+            <div className={"search-bar"}><input type="text" placeholder="Search.."></input></div>
+                <div className={"flex-container"}>
+                    <div className={"flex-item1"}>Animals</div>
+                    <div className={"flex-item2"}>Geometry</div>
+                    <div className={"flex-item3"}>Vehicles</div>
+                    <div className={"flex-item4"}>Characters</div>
+                    <div className={"flex-item5"}>Architecture</div>
+                </div>
         </>
     );
 }
